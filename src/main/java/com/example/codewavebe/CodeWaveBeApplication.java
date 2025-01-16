fff77bb6942a58a568dcbcbe6483728602f50752
Chaemin-Lim (임채민) <antraxmin@naver.com>
2025-01-16 18:44:43 +0900

    Initial Commit

---
+++ src/main/java/com/example/codewavebe/CodeWaveBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.codewavebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeWaveBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeWaveBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codewavebe/CodeWaveBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codewavebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeWaveBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
